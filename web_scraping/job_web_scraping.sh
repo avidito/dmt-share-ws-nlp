28b0eff63b8d3fe09e6c2fcf82e164043da6050b
leonardi <av.leonardif@gmail.com>
2021-09-25 14:10:34 +0700

    add result backup

--- web_scraping/job_web_scraping.sh
+++ web_scraping/job_web_scraping.sh
@@ -9,18 +9,22 @@
 ##############################################################
 
 ##### Variables #####
-vjob_date=$1
+job_date=$1
 
-if [[ $vjob_date = '' ]]; then
-  echo "vjob_date wasn't provided. Use default value."
-  vjob_date=`date -d "1 days ago" '+%Y-%m-%d'`
+if [[ $job_date = '' ]]; then
+  echo "job_date wasn't provided. Use default value."
+  job_date=`date -d "1 days ago" '+%Y-%m-%d'`
 fi
 
 ##### Main #####
-echo "Running Scraper with date: $vjob_date"
-python /app/web_scraping/scraper/run_scraper.py $vjob_date
+echo "Running Scraper with date: $job_date"
+python /app/web_scraping/scraper/run_scraper.py $job_date
+
+echo "Backup result"
+cp /app/web_scraping/result/scraping_result.csv /backup/scraping_result-$job_date.csv
 
 echo "Loading Scraper result to database"
 bash /app/web_scraping/loader/run_loader.sh
+rm /app/web_scraping/result/scraping_result.csv
 
 exit 0
