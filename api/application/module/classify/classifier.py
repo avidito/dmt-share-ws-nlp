988d66b2fe55bc41e8bdfd824233392940eec248
leonardi <av.leonardif@gmail.com>
2021-10-28 10:48:20 +0700

    update be and fe for cors

--- api/application/module/classify/classifier.py
+++ api/application/module/classify/classifier.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 from google.cloud import storage
 storage_client = storage.Client()
@@ -27,6 +28,10 @@ word_tokens = [
 tokenizer = Tokenizer(num_words=vocab_size)
 tokenizer.fit_on_texts(word_tokens)
 
+# Initialize Stemmer
+stem = StemmerFactory()
+stemmer = stem.create_stemmer()
+
 model = None
 
 def classify_title(title):
@@ -39,7 +44,9 @@ def classify_title(title):
     maxlen = 19
     category = ["entertainment", "lifestyle", "sport", "technology"]
 
-    padded_token = pad_sequences(tokenizer.texts_to_sequences([title]), maxlen=maxlen)
+    clean_title = remove_stopwords(cleaning_text(title))
+    stemmed_title = "".join([stemmer.stem(word.lower()) for word in title.split(" ")])
+    padded_token = pad_sequences(tokenizer.texts_to_sequences([stemmed_title]), maxlen=maxlen)
     proba = model.predict(padded_token)
 
     result = category[np.argmax(proba[0])]
@@ -47,3 +54,24 @@ def classify_title(title):
         ctg: p for ctg, p in zip(category, proba[0])
     }
     return result, ctg_proba
+
+# Preprocessing
+def cleaning_text(text):
+    clean_str = text.lower() # lowercase
+    clean_str = re.sub(r"(?:\@|#|https?\://)\S+", " ", clean_str) # eliminate username, url, hashtags
+    clean_str = re.sub(r'&amp;', '', clean_str) # remove &amp; as it equals &
+    clean_str = re.sub(r'[^\w\s]',' ', clean_str) # remove punctuation
+    clean_str = re.sub(r'[0-9]', '', clean_str) # remove number
+    clean_str = re.sub(r'[\s\n\t\r]+', ' ', clean_str) # remove extra space
+    clean_str = clean_str.strip() # trim
+
+    return clean_str
+
+def remove_stopwords(text, stopwords):
+    clean_tokens = [w for w in text.split(" ") if (w not in stopwords) and (w != "")]
+    clean_text = " ".join(clean_tokens)
+    return clean_text
+
+def stem_token(text, stemmer):
+     stemmed_token = [stemmer.stem(w) for w in text.split(" ") if (w != "")]
+     return " ".join(stemmed_token)
